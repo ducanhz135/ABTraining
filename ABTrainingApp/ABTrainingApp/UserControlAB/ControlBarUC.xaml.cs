3dc704551ec6b7bea06c6d0105931ea4959af34b
duc anh <ducanhz135@gmail.com>
2020-04-04 16:27:50 +0700

    commit

--- ABTrainingApp/ABTrainingApp/UserControlAB/ControlBarUC.xaml.cs
+++ ABTrainingApp/ABTrainingApp/UserControlAB/ControlBarUC.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ABTrainingApp.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,12 @@ namespace ABTrainingApp.UserControlAB
     /// </summary>
     public partial class ControlBarUC : UserControl
     {
+        //public ControlBarViewModel Viewmodel { get; set; }
         public ControlBarUC()
         {
+
             InitializeComponent();
+            //this.DataContext = Viewmodel = new ControlBarViewModel();
         }
     }
 }
